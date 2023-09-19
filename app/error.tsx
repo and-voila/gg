008c7007cbcb2e97d7ceddea0376fda36cfeefcd
Sam Rizvi <srizvi@users.noreply.github.com>
2023-09-19 11:25:27 -0700

    style: misc. fixes #47
    - update error page copy

--- app/error.tsx
+++ app/error.tsx
@@ -3,17 +3,16 @@
 export default function Error({ reset }: { reset: () => void }) {
   return (
     <div className="mx-auto my-4 flex max-w-xl flex-col rounded-lg border p-8 md:p-12">
-      <h2 className="text-xl font-bold">Whoops, we smoked it!</h2>
+      <h2 className="text-xl font-bold">Oops, a minor glitch!</h2>
       <p className="my-2">
-        Looks like we overcooked something in our digital kitchen. No worries,
-        these things happen when you&apos;re playing with fire. Give it another
-        go, will you?
+        Seems like we hit a minor roadblock. Don&apos;t worry, even the best
+        marketing strategies sometimes need a tweak or two. Try a do over?
       </p>
       <button
         className="mx-auto mt-4 flex w-full items-center justify-center rounded-xl bg-muted-foreground p-4 tracking-wide text-foreground hover:opacity-90"
         onClick={() => reset()}
       >
-        Try again?
+        Try Again
       </button>
     </div>
   );
