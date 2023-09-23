a290c33de0a485f2bc7ab100686c534c48a40291
Sam Rizvi <srizvi@users.noreply.github.com>
2023-09-22 22:41:51 -0700

    feat: add security headers #132
    - added basic security headers for now until can figure out #130 using Edge runtime
    - added `priority` to above-fold image on hero

--- components/community/community-hero.tsx
+++ components/community/community-hero.tsx
@@ -58,6 +58,7 @@ const CommunityHero: FC = () => {
         <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
           <FadeIn className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
             <Image
+              priority
               src="/images/shared/community.jpg"
               alt="A partial screenshot of the Sizzling Squad Discord community welcome screen."
               width={2432}
